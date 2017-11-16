fc723f0e18c490d0c2de1c565264a105e706b19a
Kushalappa, Nikhil <nikhil.kushalappa@accenture.com>
2017-11-16 14:28:54 +0000

    added jshintrc and configured for node env

--- api/controllers/baseController.js
+++ api/controllers/baseController.js
@@ -2,14 +2,14 @@ module.exports.currentTime = (req, res) => {
   let d = (new Date());
   let currentTime = d.getHours() + ":" + d.getMinutes();
   res.json(currentTime);
-}
+};
 
 module.exports.currentMonth = (req, res) => {
   let currentMonth = (new Date()).getMonth()+1;
   res.json(currentMonth);
-}
+};
 
 module.exports.currentYear = (req, res) => {
   let currentYear = (new Date()).getFullYear();
   res.json(currentYear);
-}
+};
